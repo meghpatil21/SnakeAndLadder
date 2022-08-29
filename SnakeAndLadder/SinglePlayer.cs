996a67ccf193a235e581dda942d0166a4003f1f1
meghpatil21 <megh.after18@gmail.com>
2022-08-29 15:32:41 +0530

    [megh] option added

--- SnakeAndLadder/SinglePlayer.cs
+++ SnakeAndLadder/SinglePlayer.cs
@@ -10,16 +10,33 @@ namespace SnakeAndLadder
     {
         public static void SinglePlayerGame()
         {
-            int startPosiion = 0; 
+            int startPosition = 0; 
+            int position = 0;   
             
             Random random = new Random();
             int dice =random.Next(1,7);
 
             Console.WriteLine();
             Console.WriteLine("Dice has been Rolled: " +dice);
+            Console.WriteLine("Checking for Options:");
 
+            int opt=random.Next(1,4);
+            switch(opt)
+            {
+                case 1:
+                    Console.WriteLine("It's No Play: You don't advance");
+                    break;
+                case 2:
+                    Console.WriteLine("Congrats, its Ladder: You advance by "+dice);
+                    position = position+dice;
+                    break;
+                case 3:
+                    Console.WriteLine("Opps!,its a Snake: You retreat by " + dice);
+                    position = position-dice;
+                    break;
+            }
 
-
+            
 
         }
     }
